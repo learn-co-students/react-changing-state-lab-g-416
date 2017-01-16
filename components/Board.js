fce37127656d7c871b93805c9c6cd0273b8f2254
evanscloud <dijkstrascloud@gmail.com>
2017-01-16 15:40:06 -0500

    Create react tic tac toe game

--- components/Board.js
+++ components/Board.js
@@ -4,8 +4,14 @@ const Field = require('./Field');
 class Board extends React.Component {
   render () {
     const { board, onClick } = this.props;
+
     return (
-      <div>
+      <div className="board">
+        {
+          board.map( (player, i) =>
+            <Field key={i} player={player} onClick={onClick.bind(null, i)} />
+          )
+        }
       </div>
     );
   }
