bba390f394248370d9cf01a570e2f386445bfd34
Pat  Pignataro <pat6284@msn.com>
2019-05-13 15:43:58 +0000

    Done.

--- components/Board.js
+++ components/Board.js
@@ -1,14 +1,14 @@
 const React = require('react');
 const Field = require('./Field');
-
 class Board extends React.Component {
   render () {
     const { board, onClick } = this.props;
+    const fields = board.map((player, index) => <Field key={index} player={player} onClick={onClick.bind(null, index)} />)
     return (
-      <div>
+      <div className="board">
+        {fields}
       </div>
     );
   }
 }
-
 module.exports = Board;
