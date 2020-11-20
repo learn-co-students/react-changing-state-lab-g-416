08f18ba7888b9e8e068b7eb5482f60b6e9b5e60d
Tracy Ngo <tngo1206@gmail.com>
2020-11-20 12:54:41 -0800

    Done.

--- components/Board.js
+++ components/Board.js
@@ -5,8 +5,13 @@ class Board extends React.Component {
   render () {
     const { board, onClick } = this.props;
     return (
-      <div>
-      </div>
+      <div className='board'>
+       {
+         board.map((player, i) =>
+           <Field key={i} player={player} onClick={onClick.bind(null, i)} />
+         )
+       }
+     </div>
     );
   }
 }
